10ffaa947559d1b844f2e2b1926764a3abf2bd69
HueHue-dev <dev@web-hl.de>
2025-10-13 15:35:57 +0200

    feat: added requestAll function and improved types

--- src/api/ApiService.ts
+++ src/api/ApiService.ts
@@ -1,15 +1,29 @@
 import Axios, { type AxiosInstance } from 'axios';
 import {
   type AxiosCacheInstance,
-  type CacheAxiosResponse,
-  setupCache,
+  setupCache, buildWebStorage ,
 } from 'axios-cache-interceptor'
 import Bottleneck from 'bottleneck';
+import type { Faction } from '@/models/faction.ts'
+
+const ONE_HOUR = 1000 * 60 * 60;
+
+enum RequestType {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
 
 export class ApiService {
   private axiosInstance: AxiosInstance = Axios.create();
 
-  private axios: AxiosCacheInstance = setupCache(this.axiosInstance);
+  private axios: AxiosCacheInstance = setupCache(
+    this.axiosInstance,
+    {
+      storage: buildWebStorage(localStorage, 'axios-cache'),
+    }
+  );
 
   private limiter: Bottleneck;
 
@@ -29,24 +43,57 @@ export class ApiService {
     })
   }
 
-  async getFactions() {
-    return await this.request<any[]>('factions', 'GET');
+  async getFactions<T>(page: number): Promise<Faction[]> {
+    return await this.request<Faction[]>('factions', RequestType.GET, page, ONE_HOUR);
   }
 
-  private async request<T>(url: string, type: string): Promise<CacheAxiosResponse<T>> {
+  async getAllFactions<T>(): Promise<Faction[]> {
+    return await this.requestAll<Faction>('factions', RequestType.GET, ONE_HOUR)
+  }
+
+  private async request<T>(url: string, requestType: string, page: number, ttl: number): Promise<T> {
     return await this.limiter.schedule(async () => {
-      const data = await this.axios.get(this.baseUrl + url, {
-        method: type,
+      const response = await this.axios.get(this.baseUrl + url + `?page=${page}`, {
+        method: requestType,
         headers: {
           Authorization: `Bearer ${this.token}`,
           'Content-Type': 'application/json'
+        },
+        cache: {
+          ttl: ttl
         }
       });
-      if (data.status === 400) {
+      if (response.status === 400) {
         throw new Error('invalid username or token');
       }
 
-      return data;
+      return response.data.data;
     });
+  }
+
+  private async requestAll<T>(url: string, requestType: string, ttl: number): Promise<T[]> {
+    let page = 1;
+    let isLastPage = false;
+    let data: T[] = [];
+
+    while (!isLastPage) {
+      const response = await this.axios.get(this.baseUrl + url, {
+        method: requestType,
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          'Content-Type': 'application/json'
+        },
+        cache: {
+          ttl: ttl
+        }
+      });
+
+      data.push(...response.data.data);
+      const totalPages = Math.ceil(response.data.meta.total / response.data.meta.limit);
+      isLastPage = page === totalPages;
+      page++;
+    }
+
+    return data
   }
 }
---
+++ src/models/faction.ts
@@ -0,0 +1,15 @@
+export interface Faction {
+  symbole: string,
+  name: string,
+  description: string,
+  headquaters: string,
+  traits: Trait[],
+  isRecruiting: boolean,
+}
+
+export interface Trait {
+  symbole: string,
+  name: string,
+  description: string,
+}
+
