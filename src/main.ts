1aa1f33a3ceae52d1069a032b629b28b58e59bcc
HueHue-dev <dev@web-hl.de>
2025-10-14 17:33:58 +0200

    fix: fixed font and icons import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import { Quasar, Notify, Loading } from 'quasar'
 
-import '@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css'
+import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
-import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
 
 import 'quasar/src/css/index.sass'
 
